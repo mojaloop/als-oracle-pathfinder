825f265a48984199c44be9f39bdd93557408c329
msk- <mattkingston@gmail.com>
2019-05-16 14:56:43 +0100

    Added db module

--- src/db.js
+++ src/db.js
@@ -1,15 +1,65 @@
 
 'use strict';
 
+const knex = require('knex');
 
+const tableCreateStmt = `
+CREATE TABLE IF NOT EXISTS pathfinderOracleCache (
+    \`pathfinderOracleCacheId\` int(10) unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Surrogate PK',
+    \`pathfinderOracleCachePartyType\` TEXT NOT NULL COMMENT 'E.g. MSISDN or MERCH_ID',
+    \`pathfinderOracleCachePartyId\` TEXT NOT NULL COMMENT 'E.g. 2234567890 (MSISDN)',
+    \`participantId\` int(10) unsigned NOT NULL UNIQUE,
+    CONSTRAINT partyinfocache_participantid_foreign FOREIGN KEY (participantId)
+        REFERENCES participant (participantId),
+    CONSTRAINT partyinfocache_partyinfotypekeyidkey_unique_key
+        UNIQUE INDEX (pathfinderOracleCachePartyType, pathfinderOracleCachePartyId)
+)`;
 
 class Database {
-    constructor({ logger = () => {}, connUri } = {}) {
-        Object.assign(this, { logger, connUri });
+    // Config should contain:
+    // { host, user, password {, port } }
+    constructor(config) {
+        this.client = knex({
+            client: 'mysql2',
+            connection: {
+                database: 'central_ledger',
+                ...config
+            }
+        });
+        this.client.raw(tableCreateStmt);
     }
 
-    async init() {
-        return;
+    /**
+     * Check whether the database connection has basic functionality
+     *
+     * @returns {boolean}
+     */
+    async isConnected() {
+        try {
+            const result = await this.client.raw('SELECT 1 + 1 AS result');
+            if (result) {
+                return true;
+            }
+            return false;
+        } catch(err) {
+            return false;
+        }
+    }
+
+    async getPartyInfo(type, id) {
+        return this.client
+            .select()
+            .from('pathfinderOracleCache')
+            .where({ type, id });
+    }
+
+    async setPartyInfo(type, id, participantId) {
+        return this.client('pathfinderOracleCache')
+            .insert({
+                pathfinderOracleCachePartyType: type,
+                pathfinderOracleCachePartyId: id,
+                participantId
+            });
     }
 }
 

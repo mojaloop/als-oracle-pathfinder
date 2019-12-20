34c7e3eda1a9df875b5fe375b41646b289cc3d26
Vassilis Barzokas <vassilis.barzokas@modusbox.com>
2019-12-20 16:54:38 +0200

    Moved the sql init statements into a file so it can be used as an init container.

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,11 @@ FROM node:8.12.0-alpine
 
 WORKDIR /opt/als-oracle-pathfinder
 
+RUN apk update && apk add bash mysql-client
+
 COPY ./src /opt/als-oracle-pathfinder/src
 COPY ./package.json ./package-lock.json /opt/als-oracle-pathfinder/
+
 RUN npm install --production
 
 CMD ["node", "/opt/als-oracle-pathfinder/src/index.js"]
--- src/db.js
+++ src/db.js
@@ -1,22 +1,6 @@
-
 'use strict';
 
 const Knex = require('knex');
-
-
-const centralLedgerTableCreateStmts = client => client.raw(`
-CREATE TABLE IF NOT EXISTS participantMno (
-    \`participantMnoId\` int(10) unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Surrogate PK',
-    \`mobileCountryCode\` SMALLINT unsigned NOT NULL COMMENT 'The three digit code representing the MCC returned by Pathfinder',
-    \`mobileNetworkCode\` SMALLINT unsigned NOT NULL COMMENT 'The three digit code representing the MNC returned by Pathfinder',
-    \`participantId\` int(10) unsigned NOT NULL UNIQUE,
-    CONSTRAINT participantmno_participantid_foreign FOREIGN KEY (participantId)
-        REFERENCES participant (participantId),
-    CONSTRAINT participantmno_participantid_mcc_mnc_unique_key
-        UNIQUE INDEX (participantId, mobileCountryCode, mobileNetworkCode),
-    CONSTRAINT participantmno_mobilecountrycode_mobilenetworkcode_unique
-        UNIQUE (mobileCountryCode, mobileNetworkCode)
-);`);
 
 class Database {
     // Config should contain:
@@ -47,7 +31,7 @@ class Database {
 
 class CentralLedgerDatabase extends Database {
     constructor (config) {
-        super(config, centralLedgerTableCreateStmts);
+        super(config);
     }
 
     /**
